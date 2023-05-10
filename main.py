ff5004701a3136b2cf87983e63e9127ccae223a1
hajas97 <hajastomi97@gmail.com>
2023-05-10 11:18:32 +0200

    chore: add text to the app

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ import tkinter as tk
 
 root = tk.Tk()
 
+root.geometry("800x600")
+root.title("My first GUI app")
+
+label = tk.Label(root, text="Hello World!", font=('Arial', 18))
+label.pack()
+
 root.mainloop()
