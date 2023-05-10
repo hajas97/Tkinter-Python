68850a342705fd467321c7ca877de69e1781f9d3
hajas97 <hajastomi97@gmail.com>
2023-05-10 11:35:24 +0200

    feat: add textbox

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ root.geometry("800x600")
 root.title("My first GUI app")
 
 label = tk.Label(root, text="Hello World!", font=('Arial', 18))
-label.pack()
+label.pack(padx=20, pady=20)
+
+textbox = tk.Text(root, height=3, font=('Times New Roman', 16))
+textbox.pack()
+
+
+
 
 root.mainloop()
